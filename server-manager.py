5d8f069a13579292de02f4adaa18a5969d83e5e0
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-02-21 10:19:50 -0600

    Container parameters set up

--- server-manager.py
+++ server-manager.py
@@ -26,44 +26,67 @@ def load_configuration():
 		configuration = json.loads(config_json)
 		return configuration
 		
-def add_image(version):
+def add_image(version, ram):
 	configuration = load_configuration()
-	name = "docker_mc"+version
-	if {"name":"docker_mc"+version} not in configuration["images"]:
-		create_image(version)
+	name = "docker_mc-version"+version+"-ram"+ram
+	if {"name": name} not in configuration["images"] and {"ram": ram} not in configuration["images"]:
+		create_image(version, ram)
 		print(name+" created")
-		configuration["images"].append({"name":"docker_mc"+version, "version": version})
-		print(configuration)
+		configuration["images"].append({"name": name, "version": version, "ram": ram})
 		save_configuration(configuration)
 	
-def create_image(version):
+def create_image(version, ram):
 	# Copy Dockerfile
 	with open("Dockerfile", "r") as dockerfile:
 		original_dockerfile=dockerfile.read()
 		dockerfile.close()
-	with open("Dockerfile_mc"+version, "w") as dockerfile:
+	with open("Dockerfile_mc-version"+version+"-ram"+ram, "w") as dockerfile:
 		new_dockerfile = original_dockerfile.replace(original_version, version)
-		dockerfile.write(new_dockerfile)
+		new_dockerfile = new_dockerfile.replace("MAXRAM", ram)
+		dockerfile.write(new_dockerfile) 
 		dockerfile.close()
-	# os.system("docker build -t docker_mc" + version + " --file Dockerfile_mc"+version + " .")
+	# os.system("docker build -t docker_mc"+version+ram + " --file Dockerfile_mc"+version+ram + " .")
+	print("docker build -t docker_mc-version"+version+"-ram"+ram + " --file Dockerfile_mc-version"+version+"-ram"+ram + " .")
 def add_container():
 	configuration = load_configuration()
-	add_image(input("What would you like the version to be?\n"))
+	add_image(input("What would you like the version to be?\n"), input("\nHow much RAM would you like the server to use, in MiB?\n"))
+def create_container(name, mc_port, rcon_port, imageion):
+	os.system("docker run -t -d -p" + mc_port + "25565 -p" + rcon_port + "25575 --name" + name + " " + image)
 	
 def menu():
-	selection = input("""Action (type number or captalized word):
+	selection = input("""Action (type number or captalized words):
   1) Add a new CONTAINER
   2) Add an IMAGE
-  3) Configure RCON
-  4) EXIT
+  3) REMOVE an IMAGE
+  4) REMOVE a CONTAINER
+  5) Configure RCON
+  6) EXIT
 """)
 	if(selection == "1" or selection == "container"):
 		add_container()
 	elif(selection == "2" or selection == "image"):
-		add_image(input("What would you like the version to be?\n"))
-	elif(selection == "3" or selection == "rcon"):
+		add_image(input("What would you like the version to be?\n"), input("How much RAM would you like the image to use at most, in MiB?\n"))
+	elif(selection == "3" or selection == "remove image"):
+		configuration = load_configuration()
+		i = 0
+		print("Select the image you want to remove:")
+		for image in configuration["images"]:
+			i += 1
+			print("  " + str(i) + ") " + image["name"] + " - " + image["version"] + " - " + image["ram"] + " MiB")
+		print("  " + str(i + 1) + ") Cancel")
+		selection = input("")
+		if(selection.isdigit()):
+			selection = int(selection)
+			if(selection < i):
+				configuration["images"].pop(selection-1)
+				save_configuration(configuration)
+		menu()
+	elif(selection == "4" or selection == "remove container"):
+		print("Not yet implemented")
+		menu()
+	elif(selection == "5" or selection == "rcon"):
 		config_rcon()
-	elif(selection == "4" or selection == "exit"):
+	elif(selection == "6" or selection == "exit"):
 		print("Exiting")
 		exit()
 	else:
@@ -107,5 +130,11 @@ menu()
 
 """
 NOTES:
-If this program evolves into a setup script for the Docker container itself, it would be favorable to adjust the amount of RAM allocated depending on the amount of RAM accessible.     
+[ ] Addition of containers from JSON and system (port, version, name rcon_port, image)
+[ ] Removal of containers from JSON and system
+[ ] Removal of images from JSON and system
+[ ] Rcon support
+[ ] Support to accesss container's shell
+[ ] File transfer
+[ ] Abilty to start and stop containers
 """
