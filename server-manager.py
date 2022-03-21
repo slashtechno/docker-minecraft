321a01c6f33de7c7e4e4fbd7eb7cca0b11763950
slashtechno <77907286+slashtechno@users.noreply.github.com>
2022-03-21 16:55:58 -0500

    Refactored and added RCON password changing
    Co-authored-by: Glitch752 <Glitch752@users.noreply.github.com>

--- server-manager.py
+++ server-manager.py
@@ -1,5 +1,9 @@
 import os
 import json
+import re
+
+from gevent import config
+from soupsieve import select
 # Variables
 original_version="1.18.1"
 # Functions
@@ -68,88 +72,136 @@ def create_container(name, mc_port, rcon_port, image):
 		image["containers"].append({"name": name}) # Append to the image's container list the name of the new container
 	save_configuration(configuration) # Save the configuration
 	os.system("docker run -t -d -p" + mc_port + ":25565 -p" + rcon_port + ":25575 --name " + name + " " + image["name"]) # Create the container using Docker with a user generated name
+	config_rcon(name) # Start the rcon configuration
 
 def menu():
-	selection = input("""Action (type number or captalized words): 
+	selection = input("""Action (type number or captalized words as a lowercase word): 
   1) Add a new CONTAINER
   2) Add an IMAGE
-  3) REMOVE an IMAGE
-  4) REMOVE a CONTAINER
-  5) Configure RCON
-  6) EXIT
+  3) Manage CONTAINERS
+  4) Manage IMAGES
+  5) EXIT
 """) # Options to list, stop, and start containers still needs to be added
 	if(selection == "1" or selection == "container"):
 		add_container() # Start container addition
 	elif(selection == "2" or selection == "image"):
 		add_image(input("What would you like the version to be?\n"), input("How much RAM would you like the image to use at most, in MiB?\n"))
-	elif(selection == "3" or selection == "remove image"):
-		configuration = load_configuration()
-		i = 0
-		print("Select the image you want to remove:")
-		for image in configuration["images"]:
-			i += 1 # The same thing as i = i +1
-			print("  " + str(i) + ") " + image["name"] + " - " + image["version"] + " - " + image["ram"] + " MiB") # {space} 1) <image name> - <version> - <ram amount> MiB 
-		print("  " + str(i + 1) + ") Cancel") # At the end of the menu, add an option to cancel
-		selection = input("") # Ask for input without a prompt
-		if(selection.isdigit()): # If the input is a digit
-			selection = int(selection) #  Turn it into an integer
-			if(selection <= i): # If the selection is valid, aka less than i
-				confirmation = input("Are you sure you want to remove this image and every single cotnainer associated with it? If so, type \"Yes, I am sure I want to do this.\"\n")
-				if(confirmation == "Yes, I am sure I want to do this."):
-					# Dockerfile for the image should be removed
-					image = configuration["images"][selection-1] # Get the image selected
-
-					# remove the containers associated with the image
-					for i in image["containers"]:
-						os.system("docker rm -f " + image["containers"][i].image["name"])
-
-					# remove the image 
-					os.system("docker rm " + image["name"])
-
-					# remove the Dockerfile
-					os.remove(image["name"].replace("docker", "Dockerfile"))
-					
-					configuration["images"].pop(selection-1) # Remove the image selected from the list
-					save_configuration(configuration) # save the configuration
-				else:
-					print("Canceling")
-					menu()
-		menu()
-	elif(selection == "4" or selection == "remove container"): # Will resemble image removal
-		configuration = load_configuration()
-		i = 0
-		for container in configuration["containers"]:
-			i += 1
-			print("  " + str(i) + ") " + container["name"])
-		print("  " + str(i + 1) + ") Cancel")
-		selection = input("")
-		if(selection.isdigit()):
-			selection = int(selection)
-			if(selection <= i):
-				confirmation = input("Are you sure you want to remove this container? If so, type \"Yes, I am sure I want to do this.\"\n")
-				if(confirmation == "Yes, I am sure I want to do this."):
-					# remove container from docker
-					os.system("docker rm -f " + configuration["containers"][selection-1]["name"])
-					configuration["containers"].pop(selection-1)
-					save_configuration(configuration)
-				else:
-					print("Canceling")
-					menu()
-		menu()
-	elif(selection == "5" or selection == "rcon"): # Majority of rcon support is added, however intergration with configuration is required
-		config_rcon()
-	elif(selection == "6" or selection == "exit"):
+	elif(selection == "3" or selection == "containers"):
+		manage_containers()
+	elif(selection == "4" or selection == "images"):
+		manage_images()
+	elif(selection == "5" or selection == "exit"):
 		print("Exiting")
 		exit()
 	else:
 		print("Invalid selection")
 		menu()
 
+def manage_containers():
+	configuration = load_configuration()
+	i = 0
+	print("What container do you want to manage?")
+	for container in configuration["containers"]:
+		i += 1 # The same thing as i = i +1
+		print("  " + str(i) + ") " + container["name"]) # {space} 1) container name
+	print("  " + str(i + 1) + ") Cancel") # At the end of the menu, add an option to cancel
+	selection = input("") # Ask for input without a prompt
+	if(selection.isdigit()): # If the input is a digit
+		selection = int(selection) #  Turn it into an integer
+		if(selection <= i): # If the selection is valid, aka less than i
+			manage_container(configuration["containers"][selection-1])
+		else:
+			print("Canceling")
+			menu()
+
+
+def manage_container(container):
+	configuration= load_configuration
+	print("""What would you like to do with this container?
+  1) Start
+  2) Stop
+  3) Restart
+  4) Remove
+  5) Change RCON password
+  6) Transfer files
+  7) Access Shell
+  8) Cancel""")
+	selection = input("")
+	if(selection == "1" or selection == "start"):
+		containerName = container["name"]
+		os.system(f"docker start {containerName}")
+	elif(selection == "2" or selection == "stop"):
+		containerName = container["name"]
+		os.system(f"docker stop {containerName}")
+	elif(selection == "3" or selection == "restart"):
+		containerName = container["name"]
+		os.system(f"docker restart {containerName}")
+	elif(selection == "4" or selection == "remove"):
+		confirmation = input("Are you sure you want to remove this container? If so, type \"Yes, I am sure I want to do this.\"\n")
+		if(confirmation == "Yes, I am sure I want to do this."):
+			containerName = container["name"]
+			os.system(f"docker rm -f {containerName}")
+			configuration["containers"].pop(selection-1)
+			save_configuration(configuration)
+	elif(selection == "5" or selection == "rcon"):
+		config_rcon(container["name"])
+	menu()
+
+def manage_images():
+	configuration = load_configuration()
+	i = 0
+	print("Select the image you want to manage:")
+	for image in configuration["images"]:
+		i += 1 # The same thing as i = i +1
+		print("  " + str(i) + ") " + image["name"] + " - " + image["version"] + " - " + image["ram"] + " MiB") # {space} 1) <image name> - <version> - <ram amount> MiB 
+	print("  " + str(i + 1) + ") Cancel") # At the end of the menu, add an option to cancel
+	selection = input("") # Ask for input without a prompt
+	if(selection.isdigit()): # If the input is a digit
+		selection = int(selection) #  Turn it into an integer
+		if(selection <= i): # If the selection is valid, aka less than i
+			manage_image(configuration["images"][selection-1]) # Get the image selected and pass it to manage_image(image)
+		else:
+			menu()
+
+def manage_image(image):
+	configuration = load_configuration()
+	print("""What would you like to do with this image?
+  1) Remove
+  2) Cancel""")
+	selection = input("")
+	if(selection == "1" or selection == "remove"):
+		confirmation = input("Are you sure you want to remove this image? By doing so, you will remove all containers assosiated with it. If so, type \"Yes, I am sure I want to do this.\"\n")
+		if(confirmation == "Yes, I am sure I want to do this."):
+			image = configuration["images"][selection-1] # Get the image selected
+
+			# remove the containers associated with the image
+			for i in image["containers"]:
+				os.system("docker rm -f " + image["containers"][i].image["name"])
+
+			# remove the image 
+			os.system("docker rmi " + image["name"])
+
+			# remove the Dockerfile
+			os.remove(image["name"].replace("docker", "Dockerfile"))
+			
+			configuration["images"].pop(selection-1) # Subtract 1 from the selection because Python lists are zero indexed
+			save_configuration(configuration)
+	menu()
+
 # Set up rcon 
-def config_rcon():
+def change_rcon_password(container):
+	password = input("What would you like the password to be?\n")
+	print("Editing files\n")
+	# Get the current server_properties
+	os.system(f"docker cp {container}:/root/minecraft/server.properties ./server.properties")
+	with open ("server.properties", "r") as container_config_file:
+		container_config = container_config_file.read()
+		container_config_file.close()
+	new_config = re.sub(container_config, "^rcon\.password=[a-zA-Z0-9\"`'#%&,:!;\-\_\.<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+$", f"rcon.password={password}")
+def config_rcon(container):
 	# Set up rcon 
 	# Request user input
-	print("In order to remotly access the console of the Minecraft server, rcon needs to be setup\nrcon is a protcol for remotely managing game servers")
+	print("In order to remotely access the console of the Minecraft server, rcon needs to be setup\nrcon is a protcol for remotely managing game servers")
 	password = input("What is the password you would like to set for rcon?\n")
 	# port=input("\nWhat is the port you would like to use for rcon?\n")
 	print("\nEditing files\n")
@@ -166,13 +218,13 @@ def config_rcon():
 
 	# Remove server.properties
 	print("Removing server.properties in container\n")
-	os.system("sudo docker exec docker-minecraft-container rm -rf /root/minecraft/server.properties")
+	os.system(f"sudo docker exec {container} rm -rf /root/minecraft/server.properties")
 	# Copy server.properties to /root/minecraft/server.properties
 	print("Copying server.properties to the container")
-	os.system("sudo docker cp server.properties docker-minecraft-container:/root/minecraft/server.properties")
+	os.system(f"sudo docker cp server.properties {container}:/root/minecraft/server.properties")
 	# Restart the docker container
 	print("Retarting the container\n")
-	os.system("sudo docker restart docker-minecraft-container")
+	os.system(f"sudo docker restart {container}")
 
 
 print("This program will ask for authentication in order to use sudo\n")
